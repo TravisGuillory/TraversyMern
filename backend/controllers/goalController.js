b9fc9540b6d339df666ec3505aae88cf9cb477a7
Travis Guillory <travisguillory70@gmail.com>
2022-02-10 17:46:35 -0600

    Assed DB and  CRUD functions

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
 
+const Goal = require("../models/goalModel");
+
 // Get goals 
 // @route GET /api/goals
 //@access Private
 const getGoals = asyncHandler(async (req, res) =>{
-    res.status(200).json({message: "Get goals"});
+    const goals = await Goal.find();
+    res.status(200).json(goals);
 });
 
 // Set goal 
@@ -16,7 +19,10 @@ const setGoal = asyncHandler(async (req, res) =>{
        throw new Error("Please submit a text field");
    }
    else {
-        res.status(200).json({message: "Set goal"});
+        const goal = await Goal.create({
+            text: req.body.text
+        });
+        res.status(200).json(goal);
    }
    
 });
@@ -25,14 +31,31 @@ const setGoal = asyncHandler(async (req, res) =>{
 // @route PUT /api/goal/id
 // @access Private
 const updateGoal = asyncHandler(async (req, res) =>{
-    res.status(200).json({message: `Update goal ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id);
+    if(!goal){
+        res.status(400);
+        throw new Error("Goal not found");
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, 
+        {
+            new: true,
+        });
+    res.status(200).json(updatedGoal);
 });
 
 // DELETE Goal
 // @rout DELETE /api/goal/id
 // @access Private
-const deleteGoal  = asynHandler(async(req, res) =>{
-    res.status(200).json({message: `Delete goal ${req.params.id}`});
+const deleteGoal  = asyncHandler(async(req, res) =>{
+    const goal = await Goal.findById(req.params.id);
+    if(!goal){
+        res.status(400);
+        throw new Error("Goal not found");
+    }    
+    /* const deletedGoal = await Goal.findByIdAndDelete(req.params.id); */
+    await goal.remove();
+    res.status(200).json({message: `Deleted goal ${req.params.id}`});
 });
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const {errorHandler} = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+const colors = require("colors");
 const port = process.env.PORT || 5000
 
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
@@ -11,4 +14,4 @@ app.use("/api/goals", require("./routes/goalRoutes"));
 
 
 
-app.listen(port, () => console.log(`Server started on prot ${port}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
